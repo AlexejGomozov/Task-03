8d510ce66d152065343af9d70209aaebff33b541
AlexejGomozov <65343579+AlexejGomozov@users.noreply.github.com>
2020-05-24 10:58:08 +0200

    Add files via upload

---
+++ Task27.java
@@ -0,0 +1,32 @@
+package by.htp.cycles.logic;
+
+import java.util.Scanner;
+
+// Для каждого натурального числа в промежутке от m до n вывести все делители, 
+// кроме единицы и самого числа. m и n вводятся с клавиатуры. 
+
+public class Task27 {
+	public static void task() {
+		int m, n, d;
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print(">> ");
+		m = sc.nextInt();
+		System.out.println("m = " + m);
+		System.out.print(">> ");
+		n = sc.nextInt();
+		System.out.println("n = " + n);
+
+		for (int i = m; i <= n; i++) {
+
+			System.out.println("делители числа " + i + ":");
+
+			for (int k = 2; k < n; k++) {
+				d = i / k;
+
+				if (i % k == 0 & i != k)
+					System.out.println("делитель числа " + i + " = " + d);
+			}
+		}
+	}
+}
